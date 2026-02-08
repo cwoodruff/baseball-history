8d93c64a2aa01cbd0007ac567647e75de346dcf6
Chris Woody Woodruff <cwoodruff@live.com>
2026-02-08 17:40:47 -0500

    removed aspire

--- baseball-history-aspire/AppHost.cs
+++
@@ -1,8 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var cache = builder.AddRedis("cache");
-
-builder.AddProject<Projects.baseball_history_web>("baseball-history-web")
-    .WithReference(cache);
-
-builder.Build().Run();
